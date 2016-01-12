fce559c9cd6ba1065edb7e0692c9e1b0fb6a89f7
julianitor <julitoledo90@gmail.com>
2016-01-12 15:46:00 +0100

    Add generic app handler that alows page reloading

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var webpack = require('webpack')
 var webpackDevMiddleware = require('webpack-dev-middleware')
 var webpackHotMiddleware = require('webpack-hot-middleware')
 var config = require('./webpack.config')
+var path = require('path');
 
 var app = new (require('express'))()
 var port = 3000
@@ -13,6 +14,11 @@ app.use(webpackHotMiddleware(compiler))
 app.get("/", function(req, res) {
   res.sendFile(__dirname + '/index.html')
 })
+
+app.get('*', function (req, res){
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+});
+
 
 app.listen(port, function(error) {
   if (error) {
