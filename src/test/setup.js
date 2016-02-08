0b63806b9a9474d10487aea7b13943da1741fa7a
Julian <julitoledo90@gmail.com>
2016-02-08 08:13:45 +0100

    Changed tests setup script

--- src/test/setup.js
+++ src/test/setup.js
@@ -3,3 +3,14 @@ import { jsdom } from 'jsdom'
 global.document = jsdom('<!doctype html><html><body></body></html>')
 global.window = document.defaultView
 global.navigator = global.window.navigator
+global.localStorage = {
+  removeItem: function() {
+    return;
+  },
+  getItem: function() {
+    return 'madeUpToken';
+  },
+  setItem: function() {
+    return;
+  }
+}
