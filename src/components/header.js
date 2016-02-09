c8c2375179a7c1a662acf8906aab3e780018a092
Julian <julitoledo90@gmail.com>
2016-02-09 21:03:49 +0100

    Add header test

--- src/components/header.js
+++ src/components/header.js
@@ -2,33 +2,31 @@ import React, { PropTypes, Component } from 'react'
 import { translate, Interpolate } from 'react-i18next/lib';
 import { Link } from 'react-router'
 
-class Header extends Component {
+export class Header extends Component {
   onClick(e) {
     e.preventDefault()
     this.props.logout()
   }
   render() {
-    const {title, username, t } = this.props  
+    const {title, username, t } = this.props
     return (
-      <div>
         <header>
           <h1>{t('appName')}:{title}</h1>
           {' '}
-          <Interpolate parent='p' i18nKey='content.welcome' value={username} />
+          <p>{t('content.welcome', {value: username})}</p>
           <Link to="/">{t('home')}</Link>
           {' '}
           <a href onClick={this.onClick.bind(this)}>{t('logout')}</a>
         </header>
-      </div>
-    );  
+    );
   }
 }
 
 Header.propTypes = {
-  title: PropTypes.string, 
+  title: PropTypes.string,
   username: PropTypes.string,
   logout: PropTypes.func.isRequired
 }
 
-// All given namespaces will be loaded. 
+// All given namespaces will be loaded.
 export default translate(['common', 'header'])(Header);
---
+++ src/test/components/header.js
@@ -0,0 +1,34 @@
+import expect from 'expect'
+import React from 'react'
+import TestUtils from 'react-addons-test-utils'
+import {Header} from '../../components/header'
+
+function setup() {
+  let props = {
+    t: expect.createSpy(),
+    logout: expect.createSpy()
+  }
+
+  let renderer = TestUtils.createRenderer()
+  renderer.render(<Header {...props} />)
+  let output = renderer.getRenderOutput()
+
+  return {
+    props,
+    output,
+    renderer
+  }
+}
+
+describe('Header', () => {
+  it('should render correctly', () => {
+    const { output } = setup()
+
+    expect(output.type).toBe('header')
+
+    let [ h1, a ] = output.props.children
+
+    expect(h1.type).toBe('h1')
+  })
+
+})
