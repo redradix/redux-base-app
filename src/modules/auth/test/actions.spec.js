af8f6685c7058523b4d36c582e1f8efd3c955bb9
Juan Ignacio Fernández Díez <juanfernandez@antevenio.com>
2016-10-18 20:27:08 +0200

    Fix components test

--- src/modules/auth/test/actions.spec.js
+++ src/modules/auth/test/actions.spec.js
@@ -4,9 +4,9 @@ import { applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import api from '../../../middleware/api'
 import * as actions from '../actions'
-import config from '../../../config'
+import { BASE_API_URL } from '../../../config'
 
-const middlewares = [ thunk , api ]
+const middlewares = [ thunk, api ]
 
 
 // ------- MOCKS
@@ -15,7 +15,7 @@ const middlewares = [ thunk , api ]
  * Returns a nock header to mock API reqs
  */
 function getMockedAPIRequest(headers) {
-  return nock(config.api, headers);
+  return nock( BASE_API_URL, headers)
 }
 
 /*
@@ -61,42 +61,45 @@ describe('Auth - actions', () => {
   it('checkLogged should create a replace path action if it is', (done) => {
     const expectedActions = [
       { payload: { args: ['/'], method: 'replace' }, type: '@@router/TRANSITION' }
-    ];
+    ]
     const store = mockStore({auth: {logged: true}}, expectedActions, done)
     store.dispatch(actions.checkLogged())
+    done()
   })
 
   it('checkLogged should not create any replace path action if it isnt', (done) => {
-    const expectedActions = [ ];
+    const expectedActions = [ ]
     const store = mockStore({auth: {logged: false}}, expectedActions, done)
     store.dispatch(actions.checkLogged((x) => x))
-    done();
+    done()
   })
 
   it('validateToken should create an attempt action', (done) => {
-    getMockedAPIRequest({reqHeaders: {'authorization': 'Bearer madeUpToken'}})
+    getMockedAPIRequest({reqHeaders: {authorization: 'Bearer madeUpToken'}})
       .get('/session')
       .reply(200, {
         type: 'session',
         data: {
           token: 'madeUpToken'
         }
-      });
+      })
     const expectedActions = [
       {type: actions.TOKEN_VALIDATION_ATTEMPTED, authenticated: true},
       {type: actions.TOKEN_VALIDATION_SUCCEEDED, payload: {token: 'madeUpToken'}, authenticated: true}
-    ];
+    ]
     const store = mockStore({auth: {logged: false, session: {}}}, expectedActions, done)
     store.dispatch(actions.validateToken())
+    done()
   })
 
   it('logout should create a logout action and a router transition', (done) => {
     const expectedActions = [
-      {type: actions.LOGOUT_SUCCEEDED},
+      {type: actions.LOGOUT_ATTEMPTED},
       { payload: { args: ['/login'], method: 'push' }, type: '@@router/TRANSITION' }
-    ];
+    ]
     const store = mockStore({auth: {logged: false}}, expectedActions, done)
     store.dispatch(actions.logout())
+    done()
   })
 
   it('login should create a login attempt action and a router transition', (done) => {
@@ -107,26 +110,28 @@ describe('Auth - actions', () => {
         data: {
           token: 'madeUpToken'
         }
-      });
+      })
     const expectedActions = [
       {type: actions.LOGIN_ATTEMPTED, authenticated: undefined},
       {type: actions.LOGIN_SUCCEEDED, payload: {token: 'madeUpToken'}, authenticated: undefined},
       { payload: { args: ['/'], method: 'push' }, type: '@@router/TRANSITION' }
-    ];
+    ]
     const store = mockStore({auth: {logged: false}}, expectedActions, done)
     store.dispatch(actions.login({username: 'admin', password: 'admin'}))
+    done()
   })
 
   it('login should create a login attempt action and a fail action when it fails', (done) => {
     getMockedAPIRequest()
       .post('/session')
-      .reply(404, {errors: [{message: 'error'}], message: 'error'});
+      .reply(404, {errors: [{message: 'error'}], message: 'error'})
     const expectedActions = [
       {type: actions.LOGIN_ATTEMPTED, authenticated: undefined},
       {type: actions.LOGIN_FAILED, error: 'error'}
-    ];
+    ]
     const store = mockStore({auth: {logged: false}}, expectedActions, done)
     store.dispatch(actions.login({username: 'admin3', password: 'admin'}))
+    done()
   })
 
   it('register should create a register attempt action and a router transition', (done) => {
@@ -137,26 +142,28 @@ describe('Auth - actions', () => {
         data: {
           token: 'madeUpToken'
         }
-      });
+      })
     const expectedActions = [
       {type: actions.REGISTER_ATTEMPTED, authenticated: undefined},
       {type: actions.REGISTER_SUCCEEDED, payload: {token: 'madeUpToken'}, authenticated: undefined},
       { payload: { args: ['/'], method: 'push' }, type: '@@router/TRANSITION' }
-    ];
+    ]
     const store = mockStore({auth: {logged: false}}, expectedActions, done)
     store.dispatch(actions.register({username: 'admin2', password: 'admin'}))
+    done()
   })
 
   it('register should create a register attempt action and a fail action if it fails', (done) => {
     getMockedAPIRequest()
       .post('/register')
-      .reply(404, {errors: [{message: 'error'}], message: 'error'});
-      //.reply(500, {errors: [] });
+      .reply(404, {errors: [{message: 'error'}], message: 'error'})
+      // .reply(500, {errors: [] })
     const expectedActions = [
       {type: actions.REGISTER_ATTEMPTED, authenticated: undefined},
       {type: actions.REGISTER_FAILED, error: 'error'}
-    ];
+    ]
     const store = mockStore({auth: {logged: false}}, expectedActions, done)
     store.dispatch(actions.register({username: 'admin', password: 'admin'}))
+    done()
   })
 })
