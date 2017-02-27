c27607cba935e1ab0e494152a67f06749f2e09c5
Aaron Contreras <aaron@redradix.com>
2017-02-27 21:13:42 +0100

    entities: add (normalized) merge action creator

--- src/modules/entities/actions.js
+++ src/modules/entities/actions.js
@@ -1,5 +1,24 @@
 import * as types from './action-types'
 
+import { normalize } from 'normalizr'
+
+/**
+ * Normalize entities and merge them into the state
+ * NOTE: During the rework of this module, normalized entities are stored under
+ * the 'normalized' key in the state object for this module
+ *
+ * @param {array} data Array from which entities will be extracted
+ * @param {object} schema Schema to be used for normalization
+ * @return {object} An action ready to be dispatched
+ */
+export function merge(data, schema) {
+  const normalized = normalize(data, schema)
+  return {
+    type: types.MERGE,
+    payload: { domains: { normalized } }
+  }
+}
+
 export function mergeEntities(domains) {
   return {
     type: types.MERGE,
