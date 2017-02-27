1386830c2e305abeff40c811dc5d39a98e3aa422
Aaron Contreras <aaron@redradix.com>
2017-02-27 20:16:00 +0100

    Update to latest version of entities

--- src/modules/entities/index.js
+++ src/modules/entities/index.js
@@ -1,11 +1,11 @@
 import * as actions from './action-types'
 export * from './actions'
 export * from './selectors'
-import {omit, merge} from 'lodash'
+import { omit } from 'lodash'
 import { generateReducer } from 'core/utils'
 
 const ACTIONS = {
-  [actions.MERGE]: (state, {payload: {domains}}) => merge({}, state, domains),
+  [actions.MERGE]: (state, {payload: {domains}}) => Object.assign({}, state, domains),
   [actions.UNSET]: (state, {payload: {domain, keys}}) => ({...state, [domain]: omit(state[domain], keys)}),
   [actions.UNSET_ALL]: (state, {payload: {domain}}) => omit(state, domain),
   [actions.CLEAR]: (state) => omit({}, state)
