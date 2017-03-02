16354ec302274cd37b0f9fb8dd459d81b9a54357
Aaron Contreras <aaron@redradix.com>
2017-03-02 16:56:39 +0100

    :fire: Remove filters module

--- src/modules/reducer.js
+++ src/modules/reducer.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux'
 import {reducer as formReducer} from 'redux-form'
 import {routerReducer as routing} from 'react-router-redux'
 import uiReducer, { moduleName as uiPath } from 'modules/ui'
-import filters, {init as initFilters} from 'modules/filters'
 import entitiesReducer, { moduleName as entitiesPath } from 'modules/entities'
 import dataReducer, { moduleName as dataPath } from 'modules/data'
 import communication, {init as initCommunication} from 'modules/communication'
@@ -10,7 +9,6 @@ import paginationReducer, { moduleName as paginationPath } from 'modules/paginat
 import configReducer, { moduleName as configPath } from 'modules/config'
 
 export const initializers = {
-  filters: initFilters,
   communication: initCommunication
 }
 
@@ -20,7 +18,6 @@ const rootReducer = combineReducers({
   [paginationPath]: paginationReducer,
   [uiPath]: uiReducer,
   [configPath]: configReducer,
-  filters,
   communication,
   routing,
   form: formReducer
