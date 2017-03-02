373a10fcca073383b4c658b298d73c70163c8638
Aaron Contreras <aaron@redradix.com>
2017-03-02 11:45:03 +0100

    Improve users mock to return paginated results

--- src/mocks/endpoints.js
+++ src/mocks/endpoints.js
@@ -45,13 +45,27 @@ const users = [{
   role: 'user'
 }]
 
+const size = 3
+
 /* eslint indent: 0 */
 // NOTE: This was commented to deploy the application with mock data.
 // TODO: Remove after api endpoints are working properly
 // if (process.env.NODE_ENV === 'development') {
   fetchMock.get(`${process.env.REACT_APP_API_URL}api/data/initial`, {data: {}})
   fetchMock.get(`${process.env.REACT_APP_API_URL}api/session`,  {type: 'session', data: {user: {name: 'miguel', surname: 'martin', email: 'a@a.com', role: 'admin'}}})
-  fetchMock.get(`${process.env.REACT_APP_API_URL}api/user/list`, { type: 'list', data: users })
+  fetchMock.get(`begin:${process.env.REACT_APP_API_URL}api/user/list`, function(url) {
+    const [, search] = url.split('?')
+    let page = 0
+    if (search) page = parseInt(search.split('=')[1], 10)
+    const start = page * size
+    const end = start + size
+    return {
+      type: 'list',
+      data: users.slice(start, end),
+      total: users.length,
+      size
+    }
+  })
   // fetchMock.delete(`${process.env.REACT_APP_API_URL}api/session`, {type: 'session', data: []})
   fetchMock.post(`${process.env.REACT_APP_API_URL}api/session`, {type: 'session', data: {token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibWlndWVsIiwic3VybmFtZSI6Im1hcnRpbiIsImVtYWlsIjoiYUBhLmNvbSIsImlhdCI6MTQ4NzcwMTEyOCwiZXhwIjoxNDg3NzI5OTI4fQ.SUUccKC13c_gdlxUf5FN1o4xeIxF9lyWSJNn3N0PNiw'}})
   .catch((unmatchedUrl, options) => {
