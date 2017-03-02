d52edf1e34f5737f361032de78a6c4efac790b3c
Aaron Contreras <aaron@redradix.com>
2017-03-02 12:49:21 +0100

    Add users selectors for pagination info

--- src/services/users/selectors.js
+++ src/services/users/selectors.js
@@ -1,10 +1,14 @@
 import { getCommState } from 'modules/communication'
 import { getEntityList } from 'modules/entities'
 import { getUIElement } from 'modules/ui'
+import { getPageNumber as getPageN, getTotal } from 'modules/pagination'
 import { DOMAIN } from './'
 
 export const isUserListReady = state => getCommState(state, DOMAIN, false)
 export const getUserList = state => getEntityList(state, DOMAIN)
 export const getUser = (state, email) => getUserList(state).find(({ user }) => user.email === email)
+
+export const getPageNumber = state => getPageN(state, 'users')
+export const getTotalPages = state => getTotal(state, 'users')
 
 export const isUserCreated = state => getUIElement(state, 'myAccount', 'user', false)
