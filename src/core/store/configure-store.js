c30831de80125b0f706695d6ed6bdee5cef79019
Aaron Contreras <aaron@redradix.com>
2017-03-14 17:00:55 +0100

    Update store for new version of redux devtools

--- src/core/store/configure-store.js
+++ src/core/store/configure-store.js
@@ -1,33 +1,29 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { browserHistory } from 'react-router'
 import { routerMiddleware } from 'react-router-redux'
 import thunk from 'redux-thunk'
 import reducer from 'modules/reducer'
 
-export default function configureStore(initialState) {
-  let middleware = applyMiddleware(routerMiddleware(browserHistory), thunk)
-
-  // Dev tools and MHR configuration
-  if (process.env.NODE_ENV === 'development') {
-    if (window.devToolsExtension) {
-      middleware = compose(middleware, window.devToolsExtension())
-    }
-  }
-
+const configureStore = (initialState) => {
+  const middleware = applyMiddleware(
+    routerMiddleware(browserHistory),
+    thunk
+  )
   const store = createStore(
     reducer,
     initialState,
-    middleware
+    composeWithDevTools(middleware)
   )
 
-  if (process.env.NODE_ENV === 'development') {
-    if (module.hot) {
-      module.hot.accept('modules/reducer', () => {
-        const nextReducer = require('modules/reducer')
-        store.replaceReducer(nextReducer)
-      })
-    }
+  if (process.env.NODE_ENV === 'development' && module.hot) {
+    module.hot.accept('modules/reducer', () => {
+      const nextReducer = require('modules/reducer')
+      store.replaceReducer(nextReducer)
+    })
   }
 
   return store
 }
+
+export default configureStore
