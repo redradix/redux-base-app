7290049dad487e247a8c7cac64e7060ee1f47095
Aaron Contreras <aaron@redradix.com>
2017-03-29 16:57:16 +0200

    Add redux store proptype shape

--- src/utils/prop-types.js
+++ src/utils/prop-types.js
@@ -1,3 +1,10 @@
+import { PropTypes } from 'react'
+
+export const storeShape = PropTypes.shape({
+  subscribe: PropTypes.func.isRequired,
+  dispatch: PropTypes.func.isRequired,
+  getState: PropTypes.func.isRequired
+})
 
 export const requiredIfCondition = (boolPropName, propTypeFn) => (props, ...rest) =>
   (props[boolPropName] ? propTypeFn.isRequired : propTypeFn)(props, ...rest)
