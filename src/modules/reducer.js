76aa867391afe853470839a09f8f4fb7fb5441fa
Aaron Contreras <aaron@redradix.com>
2017-04-12 02:14:09 +0200

    :fire: Turn ui module into legacy-ui. Stop using its reducer

--- src/modules/reducer.js
+++ src/modules/reducer.js
@@ -1,17 +1,14 @@
 import { combineReducers } from 'redux'
 import {reducer as formReducer} from 'redux-form'
 import {routerReducer as routing} from 'react-router-redux'
-import uiReducer, { moduleName as uiPath } from 'modules/ui'
+import uiReducer, { moduleName as uiPath } from 'modules/ui-reborn'
 import entitiesReducer, { moduleName as entitiesPath } from 'modules/entities-with-redux-query'
 import dataReducer, { moduleName as dataPath } from 'modules/data'
 import communicationReducer, { moduleName as communicationPath } from 'modules/communication'
 import paginationReducer, { moduleName as paginationPath } from 'modules/pagination'
 import configReducer, { moduleName as configPath } from 'modules/config'
 
-import uiRebornReducer, { moduleName as uiRebornPath } from 'modules/ui-reborn'
-
 const rootReducer = combineReducers({
-  [uiRebornPath]: uiRebornReducer,
   [dataPath]: dataReducer,
   [entitiesPath]: entitiesReducer,
   [paginationPath]: paginationReducer,
